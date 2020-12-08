58e80ddb9f095663655bfa6f8a61f836c34e6723
Lan Chaofan <cflanscut@163.com>
2020-12-08 16:41:33 +0800

    finished processing image

--- task3/image_ready.py
+++ task3/image_ready.py
@@ -1,1 +1,95 @@
-print('testlinux')
+#!/home/chaofan/anaconda3/bin/python
+# -*- encoding: utf-8 -*-
+'''
+@Description:       :
+@Date     :2020/12/04 09:45:03
+@Author      :chaofan
+@version      :1.0
+'''
+import os
+import numpy as np
+import cv2
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+
+
+def readfile(path, label_needed=False):
+    """
+    @description  :tranform the raw data into vectors
+    ---------
+    @param  :path is the input system path, label_needed means that the inputs
+     and outputs have labels
+    -------
+    @Returns  :x(return picture vector),y(return picture's label if neccessary)
+    -------
+    """
+    image_dir = sorted(
+        os.listdir(path))  # 输入path，范围该path下的dir，即所有文件名，并用sorted排序
+    x = np.zeros((len(image_dir), 128, 128, 3),
+                 dtype=np.uint8)  # 根据path下面的长度构造容器长度
+    y = np.zeros((len(image_dir)), dtype=np.uint8)
+    for i, file in enumerate(image_dir):
+        img = cv2.imread(os.path.join(path, file))  # 用相对路径访问文件
+        x[i, :, :] = cv2.resize(img, (128, 128))
+        if label_needed:
+            y[i] = int(file.split("_")[0])
+    if label_needed:
+        return x, y
+    else:
+        return x
+
+
+workspace_dir = './task3/food-11'
+print("Reading data")
+train_x, train_y = readfile(os.path.join(workspace_dir, 'training'), True)
+print("Size of training data={}".format(len(train_x)))
+val_x, val_y = readfile(os.path.join(workspace_dir, 'validation'), True)
+print("Size of validation data={}".format(len(val_x)))
+test_x = readfile(os.path.join(workspace_dir, 'testing'))
+print("Size of testing data={}".format(len(test_x)))
+
+train_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomHorizontalFlip(),  # 随机翻转
+    transforms.RandomRotation(15),  # 随机旋转
+    transforms.ToTensor(),  # 把图片转成tensor，并normalization
+])  # Compose是一个类，存储了变换的指令和函数
+test_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.ToTensor(),
+])
+
+
+class ImgDataset(Dataset):
+    def __init__(self, x, y=None, transform=None):
+        self.x = x
+        self.y = y
+        if y is not None:
+            self.y = torch.LongTensor(y)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, index):
+        X = self.x[index]
+        if self.transform is not None:
+            X = self.transform(X)
+        if self.y is not None:
+            Y = self.y[index]
+            return X, Y
+        else:
+            return X
+
+
+batch_size = 128
+train_set = ImgDataset(train_x, train_y, train_transform)
+val_set = ImgDataset(val_x, val_y, test_transform)
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
+
+torch.save(train_set, './task3/train_set.pth')
+torch.save(val_set, './task3/val_set.pth')
+torch.save(train_loader, './task3/train_loader.pth')
+torch.save(val_loader, './task3/val_loader.pth')
